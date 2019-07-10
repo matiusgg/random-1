32438b83a7409918851a29c9e43e4cb2449cecb0
enmodoflow <enmodoflow@gmail.com>
2019-07-10 11:54:10 +0200

    que viene el chupacabra primo

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+
+$peña = [
+    'Julia',
+    'Jorge',
+    'Jose',
+    'Mateo',
+    'Jaume',
+    'Petro',
+    'Alejandro',
+    'Fran',
+    'Adri',
+    'Lolo',
+    'Cristian',
+    'Alexis',
+    'David',
+    'Jordi',
+    'Luis',
+    'Toni'
+];
+$julia = 'Julia';
+$jorge = 'Jorge';
+$jose = 'Jose';
+$mateo = 'Mateo';
+$jaume = 'Jaume';
+$petro = 'Petro';
+$alejandro = 'Alejandro';
+$fran = 'Fran';
+$adri = 'Adri';
+$lolo = 'Lolo';
+$cristian = 'Cristian';
+$alexis = 'Alexis';
+$david = 'David';
+$jordi = 'Jordi';
+$luis = 'Luis';
+$toni = 'Toni';
+
+
+
+echo($random = rand(0,15));
+
+if($random = 0){
+    echo('Julia');
+}
+else if($random = 1){
+    echo('Jorge');
+}
+else if($random = 2){
+    echo('Jose');
+}
+else if($random = 3){
+    echo('Mateo');
+}
+else if($random = 4){
+    echo('Jaume');
+}
+else if($random = 5){
+    echo('Petro');
+}
+else if($random = 6){
+    echo('Alejandro');
+}
+else if($random = 7){
+    echo('Fran');
+}
+else if($random = 8){
+    echo('Adri');
+}
+else if($random = 9){
+    echo('Lolo');
+}
+else if($random = 10){
+    echo('Cristian');
+}
+else if($random = 11){
+    echo('Alexis');
+}
+else if($random = 12){
+    echo('David');
+}
+
+
+
+
+
+
+
+
+
+// $julia,$jorge,$jose,$mateo,$jaume,$petro,$alejandro,$fran,$adri,$lolo,$cristian,$alexis,$david,$jordi,$luis,$toni
